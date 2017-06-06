f7ce05f5a798abcd99639cd455feb78a20a76a5b
MorrisB-- <mo_mann@ymail.com>
2017-06-05 23:01:05 -0500

    Cleaning up code

--- src/com/masterhash/gui/Welcome.java
+++ src/com/masterhash/gui/Welcome.java
@@ -2,8 +2,6 @@ package com.masterhash.gui;
 
 import java.io.File;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -18,18 +16,17 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class Welcome extends Application implements EventHandler<ActionEvent> {
-	Stage window;
-	File file; // Creating class variable 
+public class Welcome extends Application {
+	static Stage window;
+	static File file;
     
 	// Defining event components
-	private TextField userNameTextField = new TextField();
-    private PasswordField passwordTextField = new PasswordField();
-	private TextField fileLocationTextField = new TextField();
-	private Button chooseFileButton = new Button("Choose File");
-	private Button submitButton = new Button("Submit");
-    private Button signUpButton = new Button("Sign Up");
-    private FileChooser fc = new FileChooser();
+	private static TextField usernameTextField = new TextField();
+    private static PasswordField passwordTextField = new PasswordField();
+	private static TextField fileLocationTextField = new TextField();
+	private static Button submitButton = new Button("Submit");
+    private static Button signUpButton = new Button("Sign Up");
+    private static FileChooser fileChooser = new FileChooser();
 	
     
 	public static void main(String[] args) {
@@ -44,56 +41,15 @@ public class Welcome extends Application implements EventHandler<ActionEvent> {
 		window.setTitle("MasterHash");
 		VBox vBox = new VBox(10);
 		
-		
-		// Setting the main title
-		Label masterHashTitle = new Label("Master Hash");
-		    masterHashTitle.setPadding(new Insets(10));
-		    masterHashTitle.setFont(Font.font(null, FontWeight.BOLD, 60));
-		  
-		    
-		// Setting up the username fields 
-		VBox usernameBox = new VBox();
-    		Label username = new Label("Username:");
-    	    userNameTextField.setMaxWidth(300);
-		usernameBox.getChildren().addAll(username, userNameTextField);
-		usernameBox.setAlignment(Pos.CENTER);
-		
-		
-		// Setting up the password fields
-		VBox passwordBox = new VBox();
-		    Label password = new Label("Password:");
-		    passwordTextField.setMaxWidth(300);
-        passwordBox.getChildren().addAll(password, passwordTextField);
-        passwordBox.setAlignment(Pos.CENTER);
-        
-        
-        // Setting up the file chooser
-		VBox fileBox = new VBox();
-		    HBox chooseFileBox = new HBox(10);
-        		Label fileLocation = new Label("File location:");
-        		    fileLocationTextField.setPrefSize(210, 20);
-        		    fileLocationTextField.setMinHeight(25);
-        		    fileLocationTextField.setMaxWidth(210);		
-        		    fileLocationTextField.setEditable(false);
-        		    chooseFileButton.setPrefWidth(80);
-        		    chooseFileButton.setMaxSize(100, 50);	  
-            		chooseFileButton.setOnAction(this);
-    		chooseFileBox.getChildren().addAll(fileLocationTextField, chooseFileButton);
-    		chooseFileBox.setAlignment(Pos.TOP_CENTER);		
-		fileBox.getChildren().addAll(fileLocation, chooseFileBox);
-		fileBox.setAlignment(Pos.CENTER);
-		
-		
-		// Setting up the submit and signup buttons
-		HBox submitButtons = new HBox(10);
-		submitButtons.getChildren().addAll(submitButton, signUpButton);
-		submitButtons.setAlignment(Pos.CENTER);
-		
+		// Setting up the components
+		Label masterHashTitle = setupMasterHashTitle();
+		VBox usernameAndPasswordBox = setupUsernameAndPasswordBox();
+		VBox fileBox = setupFileBox();
+		HBox signUpAndSubmitButtons = setupSignUpAndSubmitButtons();
 		
 		// Adding componenets to main layout box
-		vBox.getChildren().addAll(masterHashTitle, usernameBox, passwordBox, fileBox, submitButtons);
+		vBox.getChildren().addAll(masterHashTitle, usernameAndPasswordBox, fileBox, signUpAndSubmitButtons);
 		vBox.setAlignment(Pos.TOP_CENTER);
-		
 		
 		// Setting the scene
 		Scene scene = new Scene(vBox, 400, 400);
@@ -104,29 +60,67 @@ public class Welcome extends Application implements EventHandler<ActionEvent> {
 
 	}
 	
-	public void handle(ActionEvent event){
-	    Object source = event.getSource();
-	  
-	    // the chooseFileButton was pressed
-	    if(source == chooseFileButton){
-	        File chosenFile = fc.showOpenDialog(window);
-	        if(chosenFile != null){
-	            fileLocationTextField.setText(chosenFile.getPath());
-	            file = chosenFile;
-	        }
-	        
-	    }
-	    
-	    
-	    // the submitButton was pressed
-	    else if(source == submitButton){          
-            // do stuff here           
-        }
-        
-	    
-        // the signUpButton was pressed
-	    else if(source == signUpButton){            
-            // do stuff here  
+	private static Label setupMasterHashTitle() {
+		Label masterHashTitle = new Label("Master Hash");
+
+		masterHashTitle.setPadding(new Insets(10));
+		masterHashTitle.setFont(Font.font(null, FontWeight.BOLD, 60));
+
+		return masterHashTitle;
+	}
+	
+	private static VBox setupUsernameAndPasswordBox() {
+		VBox usernameAndPasswordBox = new VBox();
+		Label username = new Label("Username:");
+		Label password = new Label("Password:");
+
+		usernameTextField.setMaxWidth(300);
+		passwordTextField.setMaxWidth(300);
+
+		usernameAndPasswordBox.getChildren().addAll(username, usernameTextField, password, passwordTextField);
+		usernameAndPasswordBox.setAlignment(Pos.CENTER);
+
+		return usernameAndPasswordBox;
+	}
+	
+	private static VBox setupFileBox() {
+		VBox fileBox = new VBox();
+		HBox chooseFileBox = new HBox(10);
+		Label fileLocation = new Label("File location:");
+		Button chooseFileButton = new Button("Choose File");
+
+		fileLocationTextField.setPrefSize(210, 20);
+		fileLocationTextField.setMinHeight(25);
+		fileLocationTextField.setMaxWidth(210);
+		fileLocationTextField.setEditable(false);
+		
+		chooseFileButton.setPrefWidth(80);
+		chooseFileButton.setMaxSize(100, 50);
+		chooseFileButton.setOnAction(e -> chooseFile());
+		chooseFileBox.getChildren().addAll(fileLocationTextField, chooseFileButton);
+		chooseFileBox.setAlignment(Pos.TOP_CENTER);
+		
+		fileBox.getChildren().addAll(fileLocation, chooseFileBox);
+		fileBox.setAlignment(Pos.CENTER);
+		
+		return fileBox;
+		
+	}
+	
+	private static HBox setupSignUpAndSubmitButtons() {
+		HBox signUpAndSubmitButtons = new HBox(10);
+
+		signUpAndSubmitButtons.getChildren().addAll(submitButton, signUpButton);
+		signUpAndSubmitButtons.setAlignment(Pos.CENTER);
+
+		return signUpAndSubmitButtons;
+	}
+	
+	private static void chooseFile() {
+		File chosenFile = fileChooser.showOpenDialog(window);
+		if (chosenFile != null) {
+            fileLocationTextField.setText(chosenFile.getPath());
+            file = chosenFile;
         }
 	}
 	
