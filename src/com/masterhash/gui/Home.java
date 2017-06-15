1ff4cd5a882fd2e73d2c9eb6506351707699216a
MorrisB-- <mo_mann@ymail.com>
2017-06-15 17:40:55 -0500

    With NewLogin.java the table at the bottom is no longer needed.

--- src/com/masterhash/gui/Home.java
+++ src/com/masterhash/gui/Home.java
@@ -3,8 +3,6 @@ package com.masterhash.gui;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -12,11 +10,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -26,13 +21,12 @@ public class Home extends Application {
 
 	Stage window;
 	TableView<Table> table;
-	TextField nameInput, usernameInput, passwordInput;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		launch(args);
 	}
 
+	@SuppressWarnings("unchecked")
 	public void start(Stage primaryStage) throws Exception {
 
 		window = primaryStage;
@@ -57,59 +51,21 @@ public class Home extends Application {
 		passwordCol.setMinWidth(200);
 		passwordCol.setCellValueFactory(new PropertyValueFactory<>("password"));
 
-		nameInput = new TextField();
-		nameInput.setPromptText("Name");
-		nameInput.setPrefWidth(156);
-
-		usernameInput = new TextField();
-		usernameInput.setPromptText("Username");
-		usernameInput.setPrefWidth(156);
-
-		passwordInput = new TextField();
-		passwordInput.setPromptText("Password");
-		passwordInput.setPrefWidth(156);
-
-		Button addbutton = new Button();
-		addbutton.setText("Add");
-		addbutton.setMinWidth(80);
-		addbutton.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				clickAddButton();
-			}
-
-		});
-
 		BorderPane borderPane = new BorderPane();
 		borderPane.setLeft(titleLabel);
 		borderPane.setRight(newLoginButton);
 		borderPane.setPadding(new Insets(20, 20, 10, 20));
-
-		HBox hBox = new HBox();
-		hBox.setPadding(new Insets(10, 10, 10, 10));
-		hBox.setSpacing(10);
-		hBox.getChildren().addAll(nameInput, usernameInput, passwordInput, addbutton);
 
 		table = new TableView<>();
 		table.setItems(getTable());
 		table.getColumns().addAll(nameCol, usernameCol, passwordCol);
 
 		VBox vBox = new VBox(10);
-		vBox.getChildren().addAll(borderPane, table, hBox);
+		vBox.getChildren().addAll(borderPane, table);
 
 		Scene scene = new Scene(vBox, 600, 600);
 		window.setScene(scene);
 		window.show();
-	}
-
-	public void clickAddButton() {
-		Table table1 = new Table();
-		table1.setName(nameInput.getText());
-		table1.setUsername(usernameInput.getText());
-		table1.setPassword(passwordInput.getText());
-		table.getItems().add(table1);
-		nameInput.clear();
-		usernameInput.clear();
-		passwordInput.clear();
 	}
 
 	public ObservableList<Table> getTable() {
